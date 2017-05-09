0db07f0f2f0c455e142347790a7f10a174578b65
surmistry <mistrs8@gmail.com>
2017-05-09 00:20:04 -0400

    full functionality, minimal styling

--- weekforecaster.js
+++ weekforecaster.js
@@ -1,45 +1,73 @@
+/*
+  DEVELOPED BY SURAJ MISRTRY
 
+  www.github.com/surmistry
+*/
+require('dotenv').config()
 const express = require("express")
 const request = require("request")
 const app     = express()
 const cors    = require("cors")
 const parser  = require("xml2json")
 const PORT    = process.env.PORT || 8080
-const seed    = requite("./data.js")
+const bodyParser = require("body-parser")
+const API_KEY = process.env.API_KEY
 /*
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
-
-  8c51a795eef29f8e96c683d549e31eb0
-  c4ad7459922d47de298d39023cccb418
-  4208c5fd0ac62f0f195743f4ea52afc8
+  This API key was given as static demonstration to plug into URL in
+  browser but will not work using request packages with CORS
 */
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-const API = '4208c5fd0ac62f0f195743f4ea52afc8'
+
+const week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
 app.get("/", (req,res) => {
   res.render('index', {weekData: undefined});
 })
-/*
-    `api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7&appid=${API}`
-    */
+
 app.post("/api/weather", (req,res) => {
   let tempvars = {}
-  console.log(req.body)
+  let city = req.body.city
+  request
+    .get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=xml&units=metric&cnt=7&APPID=${API_KEY}`, function(err, response, body) {
 
-  /*request
-    .get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7&APPID=${API}`, function(err, response, body) {
+      try{
+        tempvars = JSON.parse(parser.toJson(body)).weatherdata.forecast.time
+        tempvars = tempvars.map((da) => {
+          let temp = da.temperature
+          let avg = Math.round(( Number(temp.max) + Number(temp.min) ) / 2)
+          let wobj = {
+            /*
+              Attempted a 7 day forcast starting at today's date,
+              current implementation only starts at the week's beginning
+              I would assume. On second iteration of OpenWeatherMap's API
+              I would do more research into JS Date() library and then pull
+              16 days and start the 7 day forcast fom there, but the 16 days
+              would affect the performance and increase the delay.  Maybe
+              even just (7*2) - 2 so that it only display a Mon-Mon up to a
+              Fri-Fri algorithm.
+            */
+            day: week[new Date(da.day).getDay()],
+            temperature: {
+              morn: temp.morn,
+              eve: temp.eve,
+              avg: (avg),
+              conditions: da.symbol.name
+            }
+          }
+          return wobj
+        })
+      }catch(err){
+        let tempvars = err
+        return tempvars
+      }
 
-      debugger
-      tempvars = {weekData: JSON.parse(parser.toJson(body)).weatherdata.forecast.time}
-      res.status(response.statusCode).render('index', tempvars )
-    })*/
 
-    tempvars = {weekData: seed}
-    res.render('index', tempvars)
+      res.status(response.statusCode).render('index', {weekData: tempvars, city: city} )
+    })
 
 })
 
